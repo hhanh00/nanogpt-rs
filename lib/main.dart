0a9b15eefdc1adfa0c8328fff843c93f611671fb
Hanh Huynh Huu <hanh@Hanhs-MacBook-Air---M1.local>
2025-08-19 14:16:13 +0800

    feat: download training data

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:logger/logger.dart';
 import 'package:nanogpt/src/rust/api/simple.dart';
 import 'package:nanogpt/src/rust/frb_generated.dart';
 import 'package:nanogpt/store.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+
+var logger = Logger();
 
 Future<void> main() async {
   await RustLib.init();
@@ -16,16 +22,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart'),
-        actions: [
-          IconButton(onPressed: appStore.inc, icon: Icon(Icons.plus_one))
-        ]),
-        body: Center(
-          child: Observer(builder: (context) => Text(
-            'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")} ${appStore.value} times`',
-          ),
+        appBar: AppBar(
+          title: const Text('flutter_rust_bridge quickstart'),
+          actions: [
+            IconButton(
+              onPressed: onDownloadTrainingData,
+              icon: Icon(Icons.download),
+              tooltip: "Download training data",
+            ),
+            IconButton(onPressed: appStore.inc, icon: Icon(Icons.plus_one)),
+          ],
         ),
+        body: SizedBox.shrink(),
       ),
-    ));
+    );
+  }
+
+  void onDownloadTrainingData() async {
+    final path = await getApplicationDocumentsDirectory();
+    final trainingFile = File(p.join(path.path, "training.txt"));
+    await trainingFile.create();
+    await downloadTrainingData(
+      path: trainingFile.path,
+      url:
+          "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt",
+    );
+    logger.i("Training data downloaded to ${trainingFile.path}");
   }
 }
